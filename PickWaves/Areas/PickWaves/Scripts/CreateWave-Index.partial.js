dfaac9722e25788257f5584b6ac8e9b938fe5510
ssinghal <ssinghal@86424e1d-813c-4845-8f53-7ce1feb40074>
2014-04-04 05:48:46 +0000

    Improving create wave scripts

--- PickWaves/Areas/PickWaves/Scripts/CreateWave-Index.partial.js
+++ PickWaves/Areas/PickWaves/Scripts/CreateWave-Index.partial.js
@@ -2,44 +2,81 @@
     $('#btnCreateBucket').button();
     // Positions the dialog at the passed td and opens it
     // this represents the dialog DOM object
-    function OpenDialog(td) {
-        $(this).dialog('option', {
-            position: {
-                of: td,
-                my: 'left top',
-                at: 'right bottom'
-            }
-        }).dialog("open");
-    }
-    
-    $('#matrixPartial').pickslipmatrix({
-        refreshing: function (event, ui) {
-            // Before the ajax call, close the popup just in case it is open
-            $('#divDlg').dialog("close");
-        },
-        selected: function (event, ui) {
-            // User selected a cell. Show popup and update its contents
-            var $dlg = $('#divDlg');
-            $('#dlgColDimSpan', $dlg).text($('#matrixPartial').pickslipmatrix("dimColDisplayName"));
-            $('#dlgRowDimSpan', $dlg).text($('#matrixPartial').pickslipmatrix("dimRowDisplayName"));
-            $('#dlgColDimSpanVal', $dlg).text(ui.colValue);
-            $('#dlgRowDimSpanVal', $dlg).text(ui.rowValue);
-            $('#dlgSpanPsCount', $dlg).text($(ui.td).text());
+    //function OpenDialog(td) {
+    //    $(this).dialog('option', {
+    //        position: {
+    //            of: td,
+    //            my: 'left top',
+    //            at: 'right bottom'
+    //        }
+    //    }).dialog("open");
+    //}
 
-            var isOpen = $dlg.dialog('isOpen');
+    //$('#matrixPartial').pickslipmatrix({
+    //    refreshing: function (event, ui) {
+    //        // Before the ajax call, close the popup just in case it is open
+    //        $('#divDlg').dialog("close");
+    //    },
+    //    selected: function (event, ui) {
+    //        // User selected a cell. Show popup and update its contents
+    //        var $dlg = $('#divDlg');
+    //        $('#dlgColDimSpan', $dlg).text($('#matrixPartial').pickslipmatrix("dimColDisplayName"));
+    //        $('#dlgRowDimSpan', $dlg).text($('#matrixPartial').pickslipmatrix("dimRowDisplayName"));
+    //        $('#dlgColDimSpanVal', $dlg).text(ui.colValue);
+    //        $('#dlgRowDimSpanVal', $dlg).text(ui.rowValue);
+    //        $('#dlgSpanPsCount', $dlg).text($(ui.td).text());
 
-            if (isOpen) {
-                $dlg.one('dialogclose', $.proxy(function (event, ui2) {
-                    OpenDialog.apply($(event.target), [this.td]);
-                }, { td: ui.td })).dialog('close');
-            } else {
-                OpenDialog.apply($dlg, [ui.td]);
-            }
+    //        var isOpen = $dlg.dialog('isOpen');
 
-        }
-    }).on('keypress', '.ui-state-highlight', function (e) {
+    //        if (isOpen) {
+    //            $dlg.one('dialogclose', $.proxy(function (event, ui2) {
+    //                OpenDialog.apply($(event.target), [this.td]);
+    //            }, { td: ui.td })).dialog('close');
+    //        } else {
+    //            OpenDialog.apply($dlg, [ui.td]);
+    //        }
+
+    //    }
+    //});
+
+    $('#matrixPartial').on('keypress', '.ui-state-highlight', function (e) {
         if (e.which == $.ui.keyCode.ENTER) {
             $('#frmMain').submit();
+        }
+    }).on('change', 'th select', function (e) {
+        $(e.delegateTarget).addClass('ui-state-disabled')
+            .load($(e.delegateTarget).attr('data-url'), $('input,select', $(e.delegateTarget)).serialize(),
+            $.proxy(function (responseText, textStatus, xhr) {
+                if (textStatus == "error") {
+                    alert("Sorry but there was an error: " + xhr.status + " " + xhr.statusText + ". Refresh the page.");
+                }
+                this.self.removeClass('ui-state-disabled');
+            }, { self: $(e.delegateTarget) }));
+        return true;
+    }).on('click', 'td.ui-selectable', function (e) {
+        var $dlg = $('#divDlg');
+        var isOpen = $dlg.dialog('isOpen');
+        var td = this;
+        if (isOpen) {
+            // Associate an event which will reopen the dialog when it is closed. Then close the dialog.
+            $dlg.one('dialogclose', function (e) {
+                $(this).dialog('option', {
+                    position: {
+                        of: $(td),
+                        my: 'left top',
+                        at: 'right bottom'
+                    }
+                }).dialog('open');
+            }).dialog('close');
+        } else {
+            // Just open the dialog
+            $dlg.dialog('option', {
+                position: {
+                    of: $(td),
+                    my: 'left top',
+                    at: 'right bottom'
+                }
+            }).dialog('open');
         }
     });
 
