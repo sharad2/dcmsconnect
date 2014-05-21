501ccce9d90c52411d2ab717391766f989824b49
apanwar <apanwar@86424e1d-813c-4845-8f53-7ce1feb40074>
2014-05-21 04:30:54 +0000

    #1894: Cancel pallet limit edit button added.

--- CartonAreas/Areas/CartonAreas/Scripts/Index-all.js
+++ CartonAreas/Areas/CartonAreas/Scripts/Index-all.js
@@ -1,9 +1,20 @@
 ﻿$(document).ready(function () {   
     $('div.boxContent').on('click', 'a.editPalletLimit', function () {
         var $item = $(this).closest('div');
-        $item.find('strong.palletLimit').hide();
+        $item.find('strong.palletLimit').hide();   
         $(this).hide();
         $item.find('input.tbPalletLimit').show();
         $item.find('button.btPalletLimit').show().button();
+        $item.find('a.cancelEdit').show();
+    });
+
+    $('div.boxContent').on('click', 'a.cancelEdit', function () {
+        var $item = $(this).closest('div');
+        $('.cancelEdit').hide();
+        $item.find('input.tbPalletLimit').hide();
+        $item.find('button.btPalletLimit').hide();
+        $item.find('strong.palletLimit').show();
+        $item.find('a.editPalletLimit').show();       
+        
     });
 });
