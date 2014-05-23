758dccac3bf90df260a260884a165a5aafc585a5
spandey <spandey@86424e1d-813c-4845-8f53-7ce1feb40074>
2014-05-23 09:06:16 +0000

    wip

--- CartonAreas/ViewModels/EditAddressOfBuildingViewModel.cs
+++ CartonAreas/ViewModels/EditAddressOfBuildingViewModel.cs
@@ -1,23 +1,36 @@
-﻿namespace DcmsMobile.CartonAreas.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+namespace DcmsMobile.CartonAreas.ViewModels
 {
     public class EditAddressOfBuildingViewModel
     {
+
         public string BuildingId { get; set; }
 
+        [Required(ErrorMessage = "Address Line 1 is required")]
+        [Display(Name = "Address Line 1")]
         public string Address1 { get; set; }
 
+        [Display(Name = "Address Line 2")]
         public string Address2 { get; set; }
 
+        [Display(Name = "Address Line 3")]
         public string Address3 { get; set; }
 
+        [Display(Name = "Address Line 4")]
         public string Address4 { get; set; }
 
+        [Required(ErrorMessage = "City is required")]
+        [Display(Name = "City")]
         public string City { get; set; }
 
+        [Display(Name = "State")]
         public string State { get; set; }
 
+        [Required(ErrorMessage = "Zip Code is required")]
+        [Display(Name = "Zip Code")]
         public string ZipCode { get; set; }
 
+        [Display(Name = "Country Code")]
         public string CountryCode { get; set; }
     }
 }
