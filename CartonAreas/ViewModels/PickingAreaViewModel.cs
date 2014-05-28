9f4769fb350c78b9bc7f46e53b87fe480fce0225
apanwar <apanwar@86424e1d-813c-4845-8f53-7ce1feb40074>
2014-05-28 09:00:07 +0000

    showing predefined values in the dialog box which is to be Update in the PickingArea.cshtml

--- CartonAreas/ViewModels/PickingAreaViewModel.cs
+++ CartonAreas/ViewModels/PickingAreaViewModel.cs
@@ -6,7 +6,21 @@ namespace DcmsMobile.CartonAreas.ViewModels
     {
         public IList<PickingAreaModel> PickingAreaList { get; set; }
 
-        public string BuildingId { get; set; }
+        public string BuildingId { get; set; } 
+
+        public string AreaId { get; set; }
+
+        public bool IsPickingArea { get; set; }
+
+        public bool IsShippingArea { get; set; }
+
+        public bool IsRestockArea { get; set; }
+
+        public bool LocationNumberingFlag { get; set; }
+
+        public string Description { get; set; }
+       
+      
     }
 
     public class PickingAreaModel : AreaModel
