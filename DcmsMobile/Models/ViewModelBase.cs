75dbee938304e8fe044db646af37d3d90260a971
ssinghal <ssinghal@86424e1d-813c-4845-8f53-7ce1feb40074>
2014-06-06 12:15:26 +0000

    Added jquery Mobile

--- DcmsMobile/Models/ViewModelBase.cs
+++ DcmsMobile/Models/ViewModelBase.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Web.Mvc;
 
 namespace DcmsMobile.Models
@@ -6,6 +7,7 @@ namespace DcmsMobile.Models
     /// <summary>
     /// A link to utility functions such as logn, diagnostics etc.
     /// </summary>
+    [Obsolete]
     public class UtilityLink
     {
         public string Url { get; set; }
@@ -20,11 +22,23 @@ namespace DcmsMobile.Models
     /// </summary>
     public class ViewModelBase
     {
+        [Obsolete]
         private IEnumerable<UtilityLink> _masterMenuItems;
+
+        public string LoginUrl { get; set; }
+
+        public string LogoffUrl { get; set; }
+
+        public string PasswordUrl { get; set; }
+
+        public string DiagnosticUrl { get; set; }
+
+        public string HomeUrl { get; set; }
 
         /// <summary>
         /// Contains information about login/logoff/changepassword/diagnostic links
         /// </summary>
+        [Obsolete]
         public IEnumerable<UtilityLink> MasterMenuItems
         {
             get { return _masterMenuItems; }
@@ -39,14 +53,29 @@ namespace DcmsMobile.Models
         {
             var list = new List<UtilityLink>();
 
+            // Home
+            HomeUrl = url.Action(MVC_DcmsMobile.Home.Index());
+            if (string.Compare(HomeUrl, ctx.HttpContext.Request.Url.AbsolutePath, true) == 0)
+            {
+                // Home Url not needed when we are on the home page
+                HomeUrl = null;
+            }
+
+            list.Add(new UtilityLink
+            {
+                Name = "Home",
+                Url = url.Action(MVC_DcmsMobile.Home.Index())
+            });
             if (ctx.HttpContext.User.Identity.IsAuthenticated)
             {
                 // Logoff
+                LogoffUrl = url.Action(MVC_DcmsMobile.Logon.Logoff());
                 list.Add(new UtilityLink
                 {
                     Name = string.Format("Log off {0}", ctx.HttpContext.User.Identity.Name),
                     Url = url.Action(MVC_DcmsMobile.Logon.Logoff())
                 });
+                PasswordUrl = url.Action(MVC_DcmsMobile.Logon.GetNewPassword());
                 list.Add(new UtilityLink
                 {
                     Name = "Change Password",
@@ -56,11 +85,18 @@ namespace DcmsMobile.Models
             else
             {
                 // Logon
+                this.LoginUrl = url.Action(MVC_DcmsMobile.Logon.Index());
                 list.Add(new UtilityLink
                 {
                     Name = "Login",
                     Url = url.Action(MVC_DcmsMobile.Logon.Index())
                 });
+            }
+            DiagnosticUrl = url.Action(MVC_DcmsMobile.Diagnostic.Index());
+            if (string.Compare(DiagnosticUrl, ctx.HttpContext.Request.Url.AbsolutePath, true) == 0)
+            {
+                // Diagnostic Url not needed on diagnostic page
+                DiagnosticUrl = null;
             }
             list.Add(new UtilityLink
             {
