5682739d0541e55de77955f38992140b72e76c91
ssinghal <ssinghal@86424e1d-813c-4845-8f53-7ce1feb40074>
2014-07-05 05:09:13 +0000

    Introduced Feature Folders

---
+++ DcmsMobile/DcmsMobile/Home/HomeController.cs
@@ -0,0 +1,89 @@
+﻿using DcmsMobile.Models;
+using EclipseLibrary.Mvc.Controllers;
+using System;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+
+namespace DcmsMobile.Controllers
+{
+    /// <summary>
+    /// Provides the list of menu choices and handles the postback of the selected choice
+    /// </summary>
+    /// <remarks>
+    /// Menu choices are built by reflecting all assemblies in the bin directory looking for Area information
+    /// </remarks>
+    public partial class HomeController : EclipseController
+    {
+        /// <summary>
+        /// Without the trailing /
+        /// </summary>
+        private string UrlRcBase
+        {
+            get
+            {
+                var urlRcBase = ConfigurationManager.AppSettings["RcUrl"];
+                if (!string.IsNullOrWhiteSpace(urlRcBase))
+                {
+                    urlRcBase = urlRcBase.Trim().TrimEnd('/');
+                }
+                return urlRcBase;
+            }
+        }
+        /// <summary>
+        /// It is important to order the menu choices, otherwise they can display in different order each time;
+        /// </summary>
+        /// <returns></returns>
+        public virtual ActionResult Index()
+        {
+            var model = new LauncherViewModel();
+            model.Init(this.ControllerContext, this.Url);
+            model.MenuItems = (from item in AreaItem.Areas
+                               orderby item.Order, item.Name
+                               select new MenuItem(item, Url)).ToArray();
+            model.UrlRcBase = this.UrlRcBase;
+
+            return View(this.Views.ViewNames.Index, model);
+        }
+
+        /// <summary>
+        /// Called from the main site to retrieve the list of programs available on RC
+        /// </summary>
+        /// <param name="version">This parameter exists to support evlution of this function. The value can be used to determine which format the results should be returned in</param>
+        /// <returns></returns>
+        /// <remarks>
+        /// jsonp data type is used to enable cross domain jquery requests as described in
+        /// http://www.pureexample.com/jquery/cross-domain-ajax.html
+        /// </remarks>
+        public virtual ActionResult RcItems(int version)
+        {
+            if (version != 1)
+            {
+                throw new NotSupportedException("Only version 1 is supported");
+            }
+            var query = from area in AreaItem.Areas
+                        select new
+                        {
+                            area = area.AreaName,
+                            url = string.Format("/{0}", area.AreaName)
+                        };
+
+            var sb = new StringBuilder(Request["callback"]);
+            sb.Append("(");
+            var ser = new JavaScriptSerializer();
+            ser.Serialize(query, sb);
+            sb.Append(")");
+            return new ContentResult
+            {
+                 Content = sb.ToString(),
+                 ContentType = "jsonp"
+            };
+        }
+
+    }
+}
+
+
+//<!--$Id$-->
---
+++ DcmsMobile/DcmsMobile/Logon/LogonController.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.Security;
+using DcmsMobile.Models;
+using EclipseLibrary.Mvc.Controllers;
+
+namespace DcmsMobile.Controllers
+{
+    /// <summary>
+    /// Default action for this controller is Index
+    /// </summary>
+    //[RoutePrefix("Logon")]
+    //[Route("{action=index}")]
+    public partial class LogonController : EclipseController
+    {
+        private const string OLD_PASSWORD = "LogonController_OldPassword";
+
+        public IFormsAuthenticationService FormsService { get; set; }
+        public IMembershipService MembershipService { get; set; }
+
+        protected override void Initialize(RequestContext requestContext)
+        {
+            if (FormsService == null) { FormsService = new FormsAuthenticationService(); }
+            if (MembershipService == null) { MembershipService = new AccountMembershipService(); }
+
+            base.Initialize(requestContext);
+        }
+
+        public virtual ActionResult Index(string returnUrl)
+        {
+            var model = new LogonModel
+            {
+                 ReturnUrl = returnUrl
+            };
+            return View(this.Views.Index, model);
+        }
+
+        /// <summary>
+        /// Called from the ring scanner view. Simply remembers the user name entered by the user
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public virtual ActionResult SetUser(LogonModel model)
+        {
+            if (string.IsNullOrWhiteSpace(model.UserName))
+            {
+                return RedirectToAction(MVC_DcmsMobile.Home.Index());
+            }
+            return View(this.Views.Index, model);
+        }
+
+        /// <summary>
+        /// Validates user name and password
+        /// </summary>
+        /// <returns></returns>
+        /// <remarks>
+        /// If the controller uses the recommended <c>AuthorizeEx</c> attribute, then reason is set in status messages.
+        /// It helps to communicate to the user why login is being requested. The reason is displayed only if the
+        /// user is already logged in.
+        /// </remarks>
+        [HttpPost]
+        public virtual ActionResult Login(LogonModel model)
+        {
+            Contract.Requires(model != null, "The passed model should never be null");
+
+            // If both user name empty, redirect to home page. Supports mobile behavior
+            if (string.IsNullOrWhiteSpace(model.UserName) || string.IsNullOrWhiteSpace(model.Password))
+            {
+                this.AddStatusMessage("Login cancelled");
+                return RedirectToAction(MVC_DcmsMobile.Home.Index());
+            }
+
+            try
+            {
+                bool b = MembershipService.ValidateUser(model.UserName, model.Password);
+                if (b)
+                {
+                    FormsService.SignIn(model.UserName, false);
+                    AddStatusMessage(string.Format("Logged in as {0}", model.UserName));
+                    if (string.IsNullOrEmpty(model.ReturnUrl))
+                    {
+                        return RedirectToAction(MVC_DcmsMobile.Home.Index());
+                    }
+                    else
+                    {
+                        return Redirect(model.ReturnUrl);
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Invalid username/password. Please try again.");
+                    // Empty model forces restart of login
+                    return View(this.Views.Index, new LogonModel());
+                }
+            }
+            catch (MembershipPasswordException)
+            {
+                var cpmodel = new ChangeExpiredPasswordModel
+                {
+                    UserName = model.UserName,
+                    ReturnUrl = model.ReturnUrl,
+                    //Password = model.Password
+                };
+                this.TempData[OLD_PASSWORD] = model.Password;
+                ModelState.AddModelError("", "Your password has expired. Please change it now.");
+                return View(this.Views.ChangeExpiredPassword, cpmodel);
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", ex.Message);
+                return View(this.Views.Index, model);
+            }
+        }
+
+        /// <summary>
+        /// This action method implements voluntary change of passwords. The view asks for the old password.
+        /// </summary>
+        /// <returns></returns>
+        [Authorize]
+        public virtual ActionResult GetNewPassword()
+        {
+            var cpmodel = new GetNewPasswordViewModel();
+            return View(this.Views.GetNewPassword, cpmodel);
+        }
+
+        /// <summary>
+        /// The old and new passwords are being posted. The password will be changed.
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Authorize]
+        public virtual ActionResult ChangePassword(GetNewPasswordViewModel model)
+        {
+            if (string.IsNullOrWhiteSpace(model.OldPassword))
+            {
+                // Old Password not entered. Just navigate to home page.
+                ModelState.Clear();
+                AddStatusMessage("Password not changed");
+                return RedirectToAction(MVC_DcmsMobile.Home.Index());
+            }
+
+            if (!ModelState.IsValid)
+            {
+                // The passwords do not match. Ask for new password again.
+                return View(this.Views.GetNewPassword, model);
+            }
+
+            try
+            {
+                if (MembershipService.ChangePassword(this.HttpContext.User.Identity.Name, model.OldPassword, model.NewPassword))
+                {
+                    // This is the success case
+                    this.AddStatusMessage("Your password has been changed");
+                    return RedirectToAction(MVC_DcmsMobile.Home.Index());
+                }
+
+                // We should never get here
+                this.ModelState.AddModelError("", "Your password could not be changed");
+            }
+            catch (Exception ex)
+            {
+                this.ModelState.AddModelError("", ex.Message);
+            }
+
+            // This is the error case
+            return View(this.Views.GetNewPassword, model);
+        }
+
+        /// <summary>
+        /// If password successfully changed, redirects to return Url or home page. Otherwise redirects to login page.
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public virtual ActionResult ChangeExpiredPassword(ChangeExpiredPasswordModel model)
+        {
+            if (string.IsNullOrWhiteSpace(model.NewPassword) && string.IsNullOrWhiteSpace(model.ConfirmPassword))
+            {
+                // Password not entered. Just navigate to home page.
+                return RedirectToAction(MVC_DcmsMobile.Home.Index());
+            }
+            if (!ModelState.IsValid)
+            {
+                // The passwords do not match. Ask for new password again.
+                return View(this.Views.ChangeExpiredPassword, model);
+            }
+
+            try
+            {
+                var oldPassword = this.TempData[OLD_PASSWORD] as string;
+                if (!string.IsNullOrWhiteSpace(oldPassword) && MembershipService.ChangePassword(model.UserName,
+                    oldPassword, model.NewPassword))
+                {
+                    // Password has successfully change. Authenticate the user.
+                    FormsService.SignIn(model.UserName, false);
+                    this.AddStatusMessage(string.Format("Password for {0} successfully changed", model.UserName));
+                    if (string.IsNullOrEmpty(model.ReturnUrl))
+                    {
+                        return RedirectToAction(MVC_DcmsMobile.Home.Index());
+                    }
+                    else
+                    {
+                        return Redirect(model.ReturnUrl);
+                    }
+                }
+                else
+                {
+                    // The only possible reason is that the old password did not match. Apologize and request login again.
+                    this.ModelState.AddModelError("", "Old password was not correct. Please try logging in again.");
+                    //return RedirectToAction("Index", new
+                    //{
+                    //    ReturnUrl = model.ReturnUrl
+                    //});
+                    return RedirectToAction(MVC_DcmsMobile.Logon.Index(model.ReturnUrl));
+                }
+            }
+            catch (Exception ex)
+            {
+                // We will need the old password after next post
+                this.TempData.Keep(OLD_PASSWORD);
+                this.ModelState.AddModelError("", ex.Message);
+                return View(this.Views.ChangeExpiredPassword, model);
+            }
+        }
+
+        public virtual ActionResult Logoff()
+        {
+            if (this.Session != null)
+            {
+                //Clean session if it exists
+                Session.Abandon();
+            }
+            FormsService.SignOut();
+            return RedirectToAction(MVC_DcmsMobile.Home.Index());
+        }
+
+        protected override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            var vr = filterContext.Result as ViewResult;
+            if (vr != null)
+            {
+                var model = vr.Model as ViewModelBase;
+                if (model != null)
+                {
+                    model.Init(this.ControllerContext, this.Url);
+                }
+            }
+            base.OnActionExecuted(filterContext);
+        }
+    }
+}
+
+
+
+
+//$Id$
