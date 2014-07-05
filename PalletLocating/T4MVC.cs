176bcf0b29ab1f07f4e8ccea4b72b571b92cd77e
apanwar <apanwar@86424e1d-813c-4845-8f53-7ce1feb40074>
2014-07-05 08:09:45 +0000

    Removed projectName_min_css from DcmsMobileAll

--- PalletLocating/T4MVC.cs
+++ PalletLocating/T4MVC.cs
@@ -101,6 +101,8 @@ namespace Links_PalletLocating
                     public static string Url(string fileName) { return T4MVCHelpers.ProcessVirtualPath(URLPATH + "/" + fileName); }
                 }
             
+                public static readonly string jquery_ui_css = T4MVCHelpers.IsProduction() && T4Extensions.FileExists(URLPATH + "/jquery-ui.min.css") ? Url("jquery-ui.min.css") : Url("jquery-ui.css");
+                     
             }
         
         }
