e59495fc098f3dc7f5b1502e340dfddf2cc85487
apanwar <apanwar@86424e1d-813c-4845-8f53-7ce1feb40074>
2014-07-05 08:09:45 +0000

    Removed projectName_min_css from DcmsMobileAll

--- BoxPick/T4MVC.cs
+++ BoxPick/T4MVC.cs
@@ -95,6 +95,8 @@ namespace Links_BoxPick
                     public static string Url(string fileName) { return T4MVCHelpers.ProcessVirtualPath(URLPATH + "/" + fileName); }
                 }
             
+                public static readonly string jquery_ui_css = T4MVCHelpers.IsProduction() && T4Extensions.FileExists(URLPATH + "/jquery-ui.min.css") ? Url("jquery-ui.min.css") : Url("jquery-ui.css");
+                     
             }
         
         }
