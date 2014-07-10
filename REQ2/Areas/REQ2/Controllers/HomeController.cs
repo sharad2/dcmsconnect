e76716eaf44740efef55094b7d981b683fff6415
apanwar <apanwar@86424e1d-813c-4845-8f53-7ce1feb40074>
2014-07-10 08:38:38 +0000

    added file helper for routings

--- REQ2/Areas/REQ2/Controllers/HomeController.cs
+++ REQ2/Areas/REQ2/Controllers/HomeController.cs
@@ -81,7 +81,7 @@ namespace DcmsMobile.REQ2.Areas.REQ2.Controllers
         /// </summary>
         /// <returns></returns>
         [HttpGet]
-        [Route(HomeController.ActionNameConstants.Index, Name = "DcmsConnect_REQ2")]
+        [Route(HomeController.ActionNameConstants.Index, Name = DcmsPublicRoutes.DcmsConnect_REQ2)]
         public virtual ActionResult Index()
         {
             var requests = _service.GetRequests();
