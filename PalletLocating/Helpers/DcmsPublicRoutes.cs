e48ad4702191dac25572077b7467f900a0014bf2
ssinghal <ssinghal@86424e1d-813c-4845-8f53-7ce1feb40074>
2014-08-05 05:51:25 +0000

    Added Library DcmsLibrary.Mvc

--- PalletLocating/Helpers/DcmsPublicRoutes.cs
+++ PalletLocating/Helpers/DcmsPublicRoutes.cs
@@ -6,120 +6,178 @@
 namespace DcmsMobile.Dcms.Routes {
 	internal static class DcmsPublicRoutes {
 		  // Generated code: Search DCMS
+		[System.Obsolete]
 		public const string DcmsConnect_Search1 = "DcmsConnect_Search1";
 		  // Generated code: List of Intransit Shipments
+		[System.Obsolete]
 		public const string DcmsConnect_IntransitShipmentList = "DcmsConnect_IntransitShipmentList";
 		  // Generated code: Manage PickWaves
+		[System.Obsolete]
 		public const string DcmsConnect_ManagePickWave = "DcmsConnect_ManagePickWave";
 		  // Generated code: Customer Order
+		[System.Obsolete]
 		public const string DcmsConnect_ManagePickWave1 = "DcmsConnect_ManagePickWave1";
 		  // Generated code: Create Pallets for Box Picking
+		[System.Obsolete]
 		public const string DcmsConnect_BoxPickPallet = "DcmsConnect_BoxPickPallet";
 		  // Generated code: Box Pick
+		[System.Obsolete]
 		public const string DcmsConnect_BoxPick = "DcmsConnect_BoxPick";
 		  // Generated code: Piece Replenish
+		[System.Obsolete]
 		public const string DcmsConnect_PieceReplenish = "DcmsConnect_PieceReplenish";
 		  // Generated code: Restock
+		[System.Obsolete]
 		public const string DcmsConnect_Restock = "DcmsConnect_Restock";
 		  // Generated code: Carton Restock
+		[System.Obsolete]
 		public const string DcmsConnect_Restock1 = "DcmsConnect_Restock1";
 		  // Generated code: Apply VAS to Boxes
+		[System.Obsolete]
 		public const string DcmsConnect_ApplyVasToBox = "DcmsConnect_ApplyVasToBox";
 		  // Generated code: Apply VAS to Boxes
+		[System.Obsolete]
 		public const string DcmsConnect_ApplyVasToBox1 = "DcmsConnect_ApplyVasToBox1";
 		  // Generated code: Scan To Pallet
+		[System.Obsolete]
 		public const string DcmsConnect_ScanToPallet = "DcmsConnect_ScanToPallet";
 		  // Generated code: Scan To Pallet
+		[System.Obsolete]
 		public const string DcmsConnect_ScanToPallet1 = "DcmsConnect_ScanToPallet1";
 		  // Generated code: Move Pallet
+		[System.Obsolete]
 		public const string DcmsConnect_MoveBoxPallet = "DcmsConnect_MoveBoxPallet";
 		  // Generated code: Move Pallet
+		[System.Obsolete]
 		public const string DcmsConnect_MoveBoxPallet1 = "DcmsConnect_MoveBoxPallet1";
 		  // Generated code: Customer Order Routing
+		[System.Obsolete]
 		public const string DcmsConnect_RoutingSummary = "DcmsConnect_RoutingSummary";
 		  // Generated code: Customer Order Routing
+		[System.Obsolete]
 		public const string DcmsConnect_CustomerRoutingSummary1 = "DcmsConnect_CustomerRoutingSummary1";
 		  // Generated code: Manage Appointments
+		[System.Obsolete]
 		public const string DcmsConnect_Appointment = "DcmsConnect_Appointment";
 		  // Generated code: Scan To Truck
+		[System.Obsolete]
 		public const string DcmsConnect_ScanToTruck = "DcmsConnect_ScanToTruck";
 		  // Generated code: Scan To Truck
+		[System.Obsolete]
 		public const string DcmsConnect_ScanToTruck1 = "DcmsConnect_ScanToTruck1";
 		  // Generated code: Abondon Rework
+		[System.Obsolete]
 		public const string DcmsConnect_AbandonRework = "DcmsConnect_AbandonRework";
 		  // Generated code: Bulk Update Carton
+		[System.Obsolete]
 		public const string DcmsConnect_BulkUpdateCarton = "DcmsConnect_BulkUpdateCarton";
 		  // Generated code: Carton Editor
+		[System.Obsolete]
 		public const string DcmsConnect_CartonEditor = "DcmsConnect_CartonEditor";
 		  // Generated code: Edit Carton
+		[System.Obsolete]
 		public const string DcmsConnect_EditCarton1 = "DcmsConnect_EditCarton1";
 		  // Generated code: Carton Locating
+		[System.Obsolete]
 		public const string DcmsConnect_CartonLocating = "DcmsConnect_CartonLocating";
 		  // Generated code: Carton Locating
+		[System.Obsolete]
 		public const string DcmsConnect_CartonLocating1 = "DcmsConnect_CartonLocating1";
 		  // Generated code: Carton To Pallet
+		[System.Obsolete]
 		public const string DcmsConnect_CartonToPallet = "DcmsConnect_CartonToPallet";
 		  // Generated code: Mark Rework Complete
+		[System.Obsolete]
 		public const string DcmsConnect_MarkReworkComplete = "DcmsConnect_MarkReworkComplete";
 		  // Generated code: Carton Areas
+		[System.Obsolete]
 		public const string DcmsConnect_CartonAreas = "DcmsConnect_CartonAreas";
 		  // Generated code: Manage Area
+		[System.Obsolete]
 		public const string DcmsConnect_ManageCartonArea1 = "DcmsConnect_ManageCartonArea1";
 		  // Generated code: Pull Request
+		[System.Obsolete]
 		public const string DcmsConnect_REQ2 = "DcmsConnect_REQ2";
 		  // Generated code: VAS Configuration
+		[System.Obsolete]
 		public const string DcmsConnect_VASConfigration = "DcmsConnect_VASConfigration";
 		  // Generated code: DcmsLite
+		[System.Obsolete]
 		public const string DcmsConnect_DcmsLite = "DcmsConnect_DcmsLite";
 		  // Generated code: Pallet Locating
+		[System.Obsolete]
 		public const string DcmsConnect_PalletLocating = "DcmsConnect_PalletLocating";
 		  // Generated code: Receiving
+		[System.Obsolete]
 		public const string DcmsConnect_Receving = "DcmsConnect_Receving";
 		  // Generated code: Repack
+		[System.Obsolete]
 		public const string DcmsConnect_Repack = "DcmsConnect_Repack";
 		  // Generated code: Manage Dcms Security
+		[System.Obsolete]
 		public const string DcmsConnect_Rights = "DcmsConnect_Rights";
 		  // Generated code: TODO
+		[System.Obsolete]
 		public const string DcmsConnect_SearchCustomer1 = "DcmsConnect_SearchCustomer1";
 		  // Generated code: TODO
+		[System.Obsolete]
 		public const string DcmsConnect_SearchUcc1 = "DcmsConnect_SearchUcc1";
 		  // Generated code: TODO
+		[System.Obsolete]
 		public const string DcmsConnect_SearchBoxPallet1 = "DcmsConnect_SearchBoxPallet1";
 		  // Generated code: TODO
+		[System.Obsolete]
 		public const string DcmsConnect_SearchWave1 = "DcmsConnect_SearchWave1";
 		  // Generated code: TODO
+		[System.Obsolete]
 		public const string DcmsConnect_SearchCarton1 = "DcmsConnect_SearchCarton1";
 		  // Generated code: TODO
+		[System.Obsolete]
 		public const string DcmsConnect_SearchCartonArea1 = "DcmsConnect_SearchCartonArea1";
 		  // Generated code: TODO
+		[System.Obsolete]
 		public const string DcmsConnect_SearchCartonPallet1 = "DcmsConnect_SearchCartonPallet1";
 		  // Generated code: TODO
+		[System.Obsolete]
 		public const string DcmsConnect_SearchCartonLocation1 = "DcmsConnect_SearchCartonLocation1";
 		  // Generated code: TODO
+		[System.Obsolete]
 		public const string DcmsConnect_SearchPo3 = "DcmsConnect_SearchPo3";
 		  // Generated code: TODO
+		[System.Obsolete]
 		public const string DcmsConnect_SearchPickslip1 = "DcmsConnect_SearchPickslip1";
 		  // Generated code: TODO
+		[System.Obsolete]
 		public const string DcmsConnect_SearchUpc1 = "DcmsConnect_SearchUpc1";
 		  // Generated code: TODO
+		[System.Obsolete]
 		public const string DcmsConnect_SearchStyle1 = "DcmsConnect_SearchStyle1";
 		  // Generated code: TODO
+		[System.Obsolete]
 		public const string DcmsConnect_SearchLabel1 = "DcmsConnect_SearchLabel1";
 		  // Generated code: TODO
+		[System.Obsolete]
 		public const string DcmsConnect_SearchSkuArea1 = "DcmsConnect_SearchSkuArea1";
 		  // Generated code: TODO
+		[System.Obsolete]
 		public const string DcmsConnect_SearchSkuLocation1 = "DcmsConnect_SearchSkuLocation1";
 		  // Generated code: TODO
+		[System.Obsolete]
 		public const string DcmsConnect_SearchIntransitShipment1 = "DcmsConnect_SearchIntransitShipment1";
 		  // Generated code: TODO
+		[System.Obsolete]
 		public const string DcmsConnect_SearchOutboundShipment1 = "DcmsConnect_SearchOutboundShipment1";
 		  // Generated code: TODO
+		[System.Obsolete]
 		public const string DcmsConnect_SearchCustomerSku1 = "DcmsConnect_SearchCustomerSku1";
 		  // Generated code: TODO
+		[System.Obsolete]
 		public const string DcmsConnect_SearchAppointmentNumber1 = "DcmsConnect_SearchAppointmentNumber1";
 		  // Generated code: TODO
+		[System.Obsolete]
 		public const string DcmsConnect_SearchReturnNumber1 = "DcmsConnect_SearchReturnNumber1";
 		  // Generated code: TODO
+		[System.Obsolete]
 		public const string DcmsConnect_SearchMasterBol1 = "DcmsConnect_SearchMasterBol1";
 		}
 }
