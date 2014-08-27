79496165651e12016d52771b5fd7842db119fcde
ssinghal <ssinghal@86424e1d-813c-4845-8f53-7ce1feb40074>
2014-08-27 08:44:19 +0000

    Reviewing Bootstrap

--- DcmsMobile/MainArea/Home/Index.js
+++ DcmsMobile/MainArea/Home/Index.js
@@ -4,6 +4,7 @@ These global variables must be set before this script is loaded
 _rcBaseUrl: URL of the RC website home page. This should be an absolute URL without the trailing /
 _rcItemsUrl: URL which will return the json array of RC items. This should be a relative to _rcBaseUrl and must begin with /
 */
+// Sharad 27 Aug 2014: This script is not working after conversion to bootstrap
 
 // Get a list of items available on the release candidate site and show the RC link for each of the items available
 $(document).ready(function () {
@@ -12,7 +13,8 @@ $(document).ready(function () {
         return;
     }
    
-    $('a.rclink span').text('Contacting...');
+    $('#rclinkText').text('Contacting...');
+    return;
     $.ajax(_rcBaseUrl + _rcItemsUrl, {
         dataType: 'jsonp',
         crossDomain: true
