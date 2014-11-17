fa57483ca49dd64a2adcbf5112e03870285d8855
ssinghal <ssinghal@86424e1d-813c-4845-8f53-7ce1feb40074>
2014-11-17 07:07:32 +0000

    Refactoring

--- Receiving/Areas/Receiving/Home/Repository/Carton.cs
+++ Receiving/Areas/Receiving/Home/Repository/Carton.cs
@@ -4,18 +4,6 @@ using System.ComponentModel.DataAnnotations;
 namespace DcmsMobile.Receiving.Areas.Receiving.Home.Repository
 {
 
-    internal class CartonArea
-    {
-        //Building to display
-        public string BuildingId { get; set; }
-        public string AreaId { get; set; }
-        public string ShortName { get; set; }
-        public string Description { get; set; }
-        public bool IsNumberedArea { get; set; }
-        public bool IsReceivingArea { get; set; }
-        public bool IsSpotCheckArea { get; set; }
-    }
-
     /// <summary>
     /// Encapsulates information about a carton. Derived classes exist for Intransit and Received cartons
     /// </summary>
---
+++ Receiving/Areas/Receiving/Home/Repository/CartonArea.cs
@@ -0,0 +1,15 @@
+﻿
+namespace DcmsMobile.Receiving.Areas.Receiving.Home.Repository
+{
+    internal class CartonArea
+    {
+        //Building to display
+        public string BuildingId { get; set; }
+        public string AreaId { get; set; }
+        public string ShortName { get; set; }
+        public string Description { get; set; }
+        public bool IsNumberedArea { get; set; }
+        public bool IsReceivingArea { get; set; }
+        public bool IsSpotCheckArea { get; set; }
+    }
+}
