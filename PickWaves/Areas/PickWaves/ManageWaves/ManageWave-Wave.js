ded0f178608b2a9ca74fd4c62456ca7a05d65fda
apanwar <apanwar@86424e1d-813c-4845-8f53-7ce1feb40074>
2014-11-24 08:59:41 +0000

    reviewing

--- PickWaves/Areas/PickWaves/ManageWaves/ManageWave-Wave.js
+++ PickWaves/Areas/PickWaves/ManageWaves/ManageWave-Wave.js
@@ -131,6 +131,7 @@ $(document).ready(function () {
     $('#freezeModal').on('show.bs.modal', function (e) {
         //alert($(e.relatedTarget).data('bucketid'));
         $('#hfBucketid', e.delegateTarget).val($(e.relatedTarget).data('bucketid'));
+        $('#countNotStartedBoxes', e.delegateTarget).html($(e.relatedTarget).data('countnotstartedboxes'));
     }).on('click', 'button:not([data-dismiss])', function (e) {
         //alert('submit');
         $('form', e.delegateTarget).trigger('submit');
