677389288251652728a4be4f9091c9c9020cf0dd
ssinghal <ssinghal@86424e1d-813c-4845-8f53-7ce1feb40074>
2014-11-28 05:45:54 +0000

    Better javascript intellisense

--- Inquiry/Scripts/_references.js
+++ Inquiry/Scripts/_references.js
@@ -1,4 +1,6 @@
 ﻿/// <reference path="jquery-2.1.1.js" />
+/// <reference path="bootstrap.js" />
+/// <reference path="typeahead.jquery.js" />
 
 // These paths are used by Visual studio to provide jquery intellisense
 // http://stackoverflow.com/questions/11488208/intellisense-doesnt-work-for-js-in-vs2012
