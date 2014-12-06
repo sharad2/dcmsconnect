810ff09d805f4fc9f74cbf164c04a5707e81ef3b
ssinghal <ssinghal@eclsys.com>
2014-12-06 17:44:42 +0530

    Added script to push subtrees

---
+++ GitScripts/subtree-push.sh
@@ -0,0 +1,42 @@
+# Use this script to push all subtrees to remote repositoryou should never need to run it.
+# Before running this script, make sure that the script remote-add.sh has already been run to create the remotes.
+
+cd ..
+
+git subtree push --prefix DcmsMobile dcmsmobile master
+
+git subtree push --prefix BoxManager boxmanager master
+
+git subtree push --prefix BoxPick boxpick master
+
+git subtree push --prefix CartonAreas cartonareas master
+
+git subtree push --prefix CartonManager cartonmanager master
+
+git subtree push --prefix DcmsLibrary.Mvc dcmslibmvc master
+
+git subtree push --prefix DcmsLite dcmslite master
+
+git subtree push --prefix DcmsRights dcmsrights master
+
+git subtree push --prefix EclipseLibrary.Mvc libmvc master
+
+git subtree push --prefix EclipseLibrary.Oracle liboracle master
+
+git subtree push --prefix Inquiry inquiry master
+
+git subtree push --prefix packages packages master
+
+git subtree push --prefix PalletLocating palletlocating master
+
+git subtree push --prefix PickWaves pickwaves master
+
+git subtree push --prefix PieceReplenish piecereplenish master
+
+git subtree push --prefix Receiving receiving master
+
+git subtree push --prefix Repack repack master
+
+git subtree push --prefix REQ2 req2 master
+
+git subtree push --prefix Shipping shipping master
