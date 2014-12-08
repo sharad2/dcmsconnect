22e31135d6945656f98669bf1f70efb6e4335d5f
ssinghal <ssinghal@eclsys.com>
2014-12-08 10:49:26 +0530

    Updating Git scripts

--- GitScripts/remote-add.sh
+++ GitScripts/remote-add.sh
@@ -1,40 +1,40 @@
 # This script was used to add remotes to this dcmsmobile repository
 # These remotes are necessary to Push subtree changes to the remote repository.
 
-git remote add dcmsmobile http://server/git/dcmsconnect/apps/dcmsmobile.git
+git remote add -f dcmsmobile http://server/git/dcmsconnect/apps/dcmsmobile.git
 
-git remote add packages http://server/git/dcmsconnect/libs/packages.git
+git remote add -f packages http://server/git/dcmsconnect/libs/packages.git
 
-git remote add dcmslibmvc http://server/git/dcmsconnect/libs/dcmslibmvc.git
+git remote add -f dcmslibmvc http://server/git/dcmsconnect/libs/dcmslibmvc.git
 
-git remote add libmvc http://server/git/dcmsconnect/libs/libmvc.git
+git remote add -f libmvc http://server/git/dcmsconnect/libs/libmvc.git
 
-git remote add liboracle http://server/git/dcmsconnect/libs/liboracle.git
+git remote add -f liboracle http://server/git/dcmsconnect/libs/liboracle.git
 
-git remote add inquiry http://server/git/dcmsconnect/apps/inquiry.git
+git remote add -f inquiry http://server/git/dcmsconnect/apps/inquiry.git
 
-git remote add boxmanager http://server/git/dcmsconnect/apps/boxmanager.git
+git remote add -f boxmanager http://server/git/dcmsconnect/apps/boxmanager.git
 
-git remote add boxpick http://server/git/dcmsconnect/apps/boxpick.git
+git remote add -f boxpick http://server/git/dcmsconnect/apps/boxpick.git
 
-git remote add cartonareas http://server/git/dcmsconnect/apps/cartonareas.git
+git remote add -f cartonareas http://server/git/dcmsconnect/apps/cartonareas.git
 
-git remote add cartonmanager http://server/git/dcmsconnect/apps/cartonmanager.git
+git remote add -f cartonmanager http://server/git/dcmsconnect/apps/cartonmanager.git
 
-git remote add dcmslite http://server/git/dcmsconnect/apps/dcmslite.git
+git remote add -f dcmslite http://server/git/dcmsconnect/apps/dcmslite.git
 
-git remote add dcmsrights http://server/git/dcmsconnect/apps/dcmsrights.git
+git remote add -f dcmsrights http://server/git/dcmsconnect/apps/dcmsrights.git
 
-git remote add palletlocating http://server/git/dcmsconnect/apps/palletlocating.git
+git remote add -f palletlocating http://server/git/dcmsconnect/apps/palletlocating.git
 
-git remote add piecereplenish http://server/git/dcmsconnect/apps/piecereplenish.git
+git remote add -f piecereplenish http://server/git/dcmsconnect/apps/piecereplenish.git
 
-git remote add receiving http://server/git/dcmsconnect/apps/receiving.git
+git remote add -f receiving http://server/git/dcmsconnect/apps/receiving.git
 
-git remote add repack http://server/git/dcmsconnect/apps/repack.git
+git remote add -f repack http://server/git/dcmsconnect/apps/repack.git
 
-git remote add req2 http://server/git/dcmsconnect/apps/req2.git
+git remote add -f req2 http://server/git/dcmsconnect/apps/req2.git
 
-git remote add shipping http://server/git/dcmsconnect/apps/shipping.git
+git remote add -f shipping http://server/git/dcmsconnect/apps/shipping.git
 
-git remote add pickwaves http://server/git/dcmsconnect/apps/pickwaves.git
+git remote add -f pickwaves http://server/git/dcmsconnect/apps/pickwaves.git
