be62287a6ed5caead5aab07396b6b4b7e8490224
deepak bhatt <deepakbhatt12@gmail.com>
2014-12-15 12:26:18 +0530

    working on request detail page

--- REQ2/Areas/REQ2/Home/PullRequestViewModel.cs
+++ REQ2/Areas/REQ2/Home/PullRequestViewModel.cs
@@ -28,8 +28,57 @@ namespace DcmsMobile.REQ2.Areas.REQ2.Home
 
         internal PullRequestViewModel(PullRequest entity)
         {
-
+            this.CtnResvId = entity.CtnResvId;
+            this.SourceAreaShortName = entity.SourceAreaShortName;
+            this.DestinationAreaShortName = entity.DestinationAreaShortName;
+            this.IsConversionRequest = entity.IsConversionRequest;
+            this.Priority = entity.Priority;
+            this.SaleTypeId = entity.SaleTypeId;
+            this.AllowOverPulling = entity.AllowOverPulling;
+            this.PackagingPreferance = entity.PackagingPreferance;
+            this.Remarks = entity.Remarks;
+            this.RequestedBy = entity.RequestedBy;
+            this.DateCreated = entity.DateCreated;
+            this.QuantityRequested = entity.QuantityRequested;
+            this.SourceQuality = entity.SourceQuality;
+            this.TargetQuality = entity.TargetQuality;
+            this.SourceVwhId = entity.TargetVwhId;
+            this.AssignedPieces = entity.AssignedPieces;
         }
+
+
+        public string CtnResvId { get; set; }
+
+        public string SourceAreaShortName { get; set; }
+
+        public string DestinationAreaShortName { get; set; }
+
+        public bool IsConversionRequest { get; set; }
+
+        public string Priority { get; set; }
+        public string SaleTypeId { get; set; }
+
+        public string AllowOverPulling { get; set; }
+
+        public string PackagingPreferance { get; set; }
+
+        public string Remarks { get; set; }
+
+        public string RequestedBy { get; set; }
+
+        public DateTime? DateCreated { get; set; }
+
+        public int QuantityRequested { get; set; }
+
+        public string SourceVwhId { get; set; }
+
+        public string TargetVwhId { get; set; }
+
+        public string SourceQuality { get; set; }
+
+        public string TargetQuality { get; set; }
+
+        public int AssignedPieces { get; set; }
 
         public IList<PullRequestSkuModel> SkuList { get; set; }
     }
