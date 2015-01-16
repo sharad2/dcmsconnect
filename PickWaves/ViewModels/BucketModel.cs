d48a71967303fc4f5a987216afb96bbc6da93bff
ssinghal <ssinghal@eclsys.com>
2015-01-16 15:29:57 +0530

    Improving bucket model view

--- PickWaves/ViewModels/BucketModel.cs
+++ PickWaves/ViewModels/BucketModel.cs
@@ -381,7 +381,7 @@ namespace DcmsMobile.PickWaves.ViewModels
 
         internal int CountTotalSku { get; set; }
 
-        [DisplayFormat(DataFormatString = "<span>{0:N0} SKUs are not assigned at any location.</span>", HtmlEncode = false)]
+        [DisplayFormat(DataFormatString = "{0:N0}")]
         public int CountNotAssignedSku
         {
             get
