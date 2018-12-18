166837a9666c46365b167392517a72b3825cbb52
happysharad <happysharad@icloud.com>
2018-12-18 13:51:27 +0530

    Testing on mobile emulator

--- Receiving/Global.asax.cs
+++ Receiving/Global.asax.cs
@@ -46,47 +46,47 @@ namespace DcmsMobile.Receiving
             RegisterGlobalFilters(GlobalFilters.Filters);
             RegisterRoutes(RouteTable.Routes);
 
-            #region Display Modes
-            // Sharad: Code which makes extensions .mobile.cshtml and .phone.cshtml recognizable
-            // Conditions are checked in the order they are added
-            DisplayModeProvider.Instance.Modes.Clear();
+            //#region Display Modes
+            //// Sharad: Code which makes extensions .mobile.cshtml and .phone.cshtml recognizable
+            //// Conditions are checked in the order they are added
+            //DisplayModeProvider.Instance.Modes.Clear();
 
-            // Smartphones are detected if the user agent contains one of these strings
-            var phones = new[] {
-                "Android",
-                "iPhone",
-                "iPad"
-            };
+            //// Smartphones are detected if the user agent contains one of these strings
+            //var phones = new[] {
+            //    "Android",
+            //    "iPhone",
+            //    "iPad"
+            //};
 
-            // Known Smart Phones will use the .phone.cshtml extension
-            // Deepak's phone User Agent for Samsung Galaxy Y
-            //  Mozilla/5.0 (Linux; U; Android 2.3.6; en-gb; GT-S5360 Build/GINGERBREAD) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1
-            // Ankit's phone user agent Samsung Galaxy Note
-            //  Mozilla/5.0 (Linux; U; Android 4.1.2; en-gb; GT-N7000 Build/JZO54K) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30
-            // iPhone 5 UserAgent when using Safari:
-            //   Mozilla/5.0 (iPhone; CPU iPhone OS 7_1_1 like Mac OS X) AppleWebKit/537.51.2 (KHTML, like Gecko) Version/7.0 Mobile/11D201 Safari/9537.53
-            //DisplayModeProvider.Instance.Modes.Add(new DefaultDisplayMode("phone")
+            //// Known Smart Phones will use the .phone.cshtml extension
+            //// Deepak's phone User Agent for Samsung Galaxy Y
+            ////  Mozilla/5.0 (Linux; U; Android 2.3.6; en-gb; GT-S5360 Build/GINGERBREAD) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1
+            //// Ankit's phone user agent Samsung Galaxy Note
+            ////  Mozilla/5.0 (Linux; U; Android 4.1.2; en-gb; GT-N7000 Build/JZO54K) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30
+            //// iPhone 5 UserAgent when using Safari:
+            ////   Mozilla/5.0 (iPhone; CPU iPhone OS 7_1_1 like Mac OS X) AppleWebKit/537.51.2 (KHTML, like Gecko) Version/7.0 Mobile/11D201 Safari/9537.53
+            ////DisplayModeProvider.Instance.Modes.Add(new DefaultDisplayMode("phone")
+            ////{
+            ////    ContextCondition = ctx =>  true
+            ////});
+
+            //// .mobile file is to be used only for ringscanners
+            //// RingScanner User Agent: Mozilla/4.0 (compatible; MSIE 6.0; Windows CE)
+            //DisplayModeProvider.Instance.Modes.Add(new DefaultDisplayMode("mobile")
             //{
-            //    ContextCondition = ctx =>  true
+            //    ContextCondition = ctx => string.Compare(ctx.Request.Browser.Platform, "WinCE", StringComparison.InvariantCultureIgnoreCase) == 0
             //});
 
-            // .mobile file is to be used only for ringscanners
-            // RingScanner User Agent: Mozilla/4.0 (compatible; MSIE 6.0; Windows CE)
-            DisplayModeProvider.Instance.Modes.Add(new DefaultDisplayMode("mobile")
-            {
-                ContextCondition = ctx => string.Compare(ctx.Request.Browser.Platform, "WinCE", StringComparison.InvariantCultureIgnoreCase) == 0
-            });
+            //// If a .desktop.cshtml file exists, it will be served to non phone user agents
+            //// Change this to true to use .desktp files
+            //DisplayModeProvider.Instance.Modes.Add(new DefaultDisplayMode("desktop")
+            //{
+            //    ContextCondition = ctx => false
+            //});
 
-            // If a .desktop.cshtml file exists, it will be served to non phone user agents
-            // Change this to true to use .desktp files
-            DisplayModeProvider.Instance.Modes.Add(new DefaultDisplayMode("desktop")
-            {
-                ContextCondition = ctx => false
-            });
-
-            // If no specific extension is found, simply use the .cshtml extension
-            DisplayModeProvider.Instance.Modes.Add(new DefaultDisplayMode());
-            #endregion
+            //// If no specific extension is found, simply use the .cshtml extension
+            //DisplayModeProvider.Instance.Modes.Add(new DefaultDisplayMode());
+            //#endregion
 
         }
 
